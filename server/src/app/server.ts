13b9664de0f6afa9896f7b80b2115346e4287b0e
Mike V <mvondrak@gmail.com>
2020-08-06 23:28:34 -0600

    remove db connect attempt

--- server/src/app/server.ts
+++ server/src/app/server.ts
@@ -7,15 +7,15 @@ import { take } from 'rxjs/operators';
 import { Observable } from 'rxjs/internal/Observable';
 
 import mysql, { ConnectionConfig, Connection, QueryOptions, MysqlError, FieldInfo, queryCallback, Pool, PoolConfig } from 'mysql';
-const dbConfig: ConnectionConfig = {
-  host: 'localhost',
-  port: 3306,
-  user: 'DiveMaster',
-  password: 'D1v3M4st3r!!',
-  database: 'dive_inn_test_db',
-}
-const testSql = mysql.createConnection(dbConfig);
-testSql.end();
+// const dbConfig: ConnectionConfig = {
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'DiveMaster',
+//   password: 'D1v3M4st3r!!',
+//   database: 'dive_inn_test_db',
+// }
+// const testSql = mysql.createConnection(dbConfig);
+// testSql.end();
 
 const testRxjs: Observable<boolean> = of(false);
 testRxjs.pipe(take(1)).subscribe(val => console.log('************* obsevables work'));
